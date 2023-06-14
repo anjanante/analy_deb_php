1501519e8f55a300385ec957efa5f18edb3ab4e5
anjaNante <36764520+anjanante@users.noreply.github.com>
2023-06-14 09:43:47 +0300

    Product start

---
+++ api/produit.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created By Storm Nant 2022
+ * User: Nantenaina
+ * Date: 13/06/2023
+ * Time: 17:59
+ */
+
+include("../config/db_connect.php");
+$request_method = $_SERVER["REQUEST_METHOD"];
+var_dump($request_method);
